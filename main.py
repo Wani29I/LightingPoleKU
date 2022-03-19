a507e6ea323c9647d912cc8195bbbeb42dca7d9c
JAmoMES <atipat.pa@ku.th>
2022-03-20 05:47:31 +0700

    more useful swagger

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 from routes.pm import pm
 from routes.light import light
 
-app = FastAPI()
-app.include_router(pm)
-app.include_router(light)
+app = FastAPI(
+    title="LigthingPoleApp"
+)
+app.include_router(pm,tags=['pm'])
+app.include_router(light,tags=['light'])
 
-@app.get("/")
+@app.get("/",tags=['check'])
 async def check_app():
     return {"message": "API is running."}
